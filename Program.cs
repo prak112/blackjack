87b4080666e722aceda5a8647419ce0f0f33a20f
Prakirth Govardhanam <prakirth112@gmail.com>
2023-05-03 19:45:07 +0300

    simplify code, add method

--- Program.cs
+++ Program.cs
@@ -14,92 +14,71 @@
             Deck player2Deck = new Deck();
 
             deck.GenerateCards();
-            deck.Shuffle();
+            //deck.Shuffle();
 
             /*
-            int drawLimit = 13;
-            while(drawLimit <= 13)
+            // total set of cards
+            int drawLimit = 10;
+            while(drawLimit <= 10)
             {
+                drawLimit--;
+
+                player1Deck.Cards.Add(deck.Draw());
+                player2Deck.Cards.Add(deck.Draw());
+
+                deck.Shuffle();
 
 
             }
             */
-
+            
+            // single card setup
             player1Deck.Cards.Add(deck.Draw());
-
             deck.Shuffle();
-
             player2Deck.Cards.Add(deck.Draw());
 
-            int player1Total = 0;
-            int player2Total = 0;
+            // Calculate scores using PlayerTotal method
+            int player1Total = PlayerTotal(player1Deck);
+            int player2Total = PlayerTotal(player2Deck);
 
-            //Deck[] playerDecks = { player1Deck, player2Deck };
-            //int[] playerTotals = { player1Total, player2Total };
-
-
-            //for (int i = 0; i < playerDecks.Length; i++)
-            
-                //for (int j = 0; j < playerTotals.Length; j++)
-            
-            // Player 2 calculation
-            foreach (Card card in player1Deck.Cards)
-            {
-                player1Total += card.Value;
-
-                foreach (Card.Symbols symbol in Enum.GetValues(typeof(Card.Symbols)))
-                {
-                    switch (symbol)
-                    {
-                        case Card.Symbols.Hearts:
-                            player1Total += 4;
-                            break;
-                        case Card.Symbols.Diamonds:
-                            player1Total += 3;
-                            break;
-                        case Card.Symbols.Clubs:
-                            player1Total += 2;
-                            break;
-                        case Card.Symbols.Spades:
-                            player1Total += 1;
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-
-            // Player 2 calculation
-            foreach (Card card in player2Deck.Cards)
-            {
-                player2Total += card.Value;
-
-                foreach (Card.Symbols symbol in Enum.GetValues(typeof(Card.Symbols)))
-                {
-                    switch (symbol)
-                    {
-                        case Card.Symbols.Hearts:
-                            player1Total += 4;
-                            break;
-                        case Card.Symbols.Diamonds:
-                            player1Total += 3;
-                            break;
-                        case Card.Symbols.Clubs:
-                            player1Total += 2;
-                            break;
-                        case Card.Symbols.Spades:
-                            player1Total += 1;
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-
-
+            // display scores and winner
             if (player1Total > player2Total) { Console.WriteLine($"Players Totals :\nPlayer 1: {player1Total}\nPlayer 2: {player2Total}\n--Player 1 Wins!--\n"); }
             else if (player1Total < player2Total) { Console.WriteLine($"Players Totals :\nPlayer 1: {player1Total}\nPlayer 2: {player2Total}\n--Player 2 Wins!--\n"); }
             else { Console.WriteLine($"Players Totals :\nPlayer 1: {player1Total}\nPlayer 2: {player2Total}\n--It's a DRAW!--\n"); }
+
+        }
+
+
+        // Internal Method - Player score calculation
+        public static int PlayerTotal(Deck playerDeck)
+        {
+            int playerTotal = 0;
+            foreach (Card card in playerDeck.Cards)
+            {
+                playerTotal += card.Value;
+
+                foreach (Card.Symbols symbol in Enum.GetValues(typeof(Card.Symbols)))
+                {
+                    switch (symbol)
+                    {
+                        case Card.Symbols.Hearts:
+                            playerTotal += 4;
+                            break;
+                        case Card.Symbols.Diamonds:
+                            playerTotal += 3;
+                            break;
+                        case Card.Symbols.Clubs:
+                            playerTotal += 2;
+                            break;
+                        case Card.Symbols.Spades:
+                            playerTotal += 1;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return playerTotal;
 
         }
     }
